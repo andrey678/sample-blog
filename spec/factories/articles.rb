246c299afa43fd4847edcbcf4a0614a5fd8614a0
andrey678 <drew6496@yandex.ru>
2020-07-17 19:55:18 +0300

    Update article factory

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,5 +2,15 @@ FactoryBot.define do
   factory :article do
     title { "Article title" }
     text { "Article text" }
+  #Создаём фабрику с названием article_with_comments
+  #Для создания статьи с несколькими комментариями
+    factory :article_with_comments do
+      #после создания article
+      after :create do |article, evaluator|
+        #создаём список из 3-х комментариев
+        create_list :comment, 3, article: article
+      end
+    end
+
   end
 end
