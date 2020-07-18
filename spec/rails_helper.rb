820cbb4bfd2ab27dff9800495049a6e816d05605
andrey678 <drew6496@yandex.ru>
2020-07-18 17:07:19 +0300

    Add support of gem "database_cleaner" in  rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,10 +1,14 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
+require 'spec_helper'
+require 'rspec/rails'
+require 'support/session_helper'
+require 'support/database_cleaner'
+require 'support/factory_bot'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'rspec/rails'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -69,8 +73,4 @@ end
     with.test_framework :rspec
     with.library :rails
   end
-end
-
-RSpec.configure do |config|
-  config.include FactoryBot::Syntax::Methods
 end
